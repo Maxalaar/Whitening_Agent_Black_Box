e60b2440f591ed596c4720fcf89f92c80834d2c0
Maxalaar <maxalaar@protonmail.com>
2024-03-20 11:35:44 +0100

    Simplified visualization file

--- executables/latent_space_visualization.py
+++ executables/latent_space_visualization.py
@@ -1,73 +1,78 @@
 from sklearn.manifold import TSNE
 import matplotlib
 import matplotlib.pyplot as plt
+from tkinter import Tk
 
 from utilities.dataset_handler import DatasetHandler
 from utilities.global_include import datasets_directory, sklearn_directory
 from utilities.sklearn_classifier_handler import SklearnClassifierHandler
 
 
-def plot_latent_space(x, y, color, rendering):
-    figure, axis = plt.subplots()
-    scatter = axis.scatter(x, y, c=color)
+class LatentSpaceVisualisation:
+    def __init__(self, name: str):
+        self.color_palette = matplotlib.colormaps['tab10']
+        self.latent_space_dataset_handler: DatasetHandler = DatasetHandler(datasets_directory, name)
 
-    figure_rendering, axis_rendering = plt.subplots()
-    axis_rendering.axis('off')
+        self.latent_space_data = None
+        self.action_data = None
+        self.rendering_data = None
 
-    def on_hover(event):
-        if event.inaxes == axis:
-            contains, ind = scatter.contains(event)
-            if contains:
-                index = ind['ind'][0]
-                axis_rendering.clear()
-                axis_rendering.imshow(rendering[index])
-                plt.draw()
-            else:
-                axis_rendering.clear()
-                plt.draw()
+        self.tsne_latent_space = None
+        self.latent_space_color = None
 
-    figure.canvas.mpl_connect('motion_notify_event', on_hover)
-    plt.show()
+        self.kmeans_classifier = None
+        self.latent_space_label = None
+
+    def load_data(self, number_data):
+        data = self.latent_space_dataset_handler.load(['latent_space', 'action', 'rendering'], number_data=number_data)
+        self.latent_space_data = data['latent_space']
+        self.action_data = data['action']
+        self.rendering_data = data['rendering']
+
+    def load_kmeans_classifier(self, name):
+        self.kmeans_classifier = SklearnClassifierHandler(sklearn_directory, name).load()
+
+    def kmeans_classifier_predict(self):
+        self.latent_space_label = self.kmeans_classifier.predict(self.latent_space_data)
+        self.latent_space_color = [self.color_palette(label) for label in self.latent_space_label]
+
+    def compute_tsne(self):
+        tsne = TSNE(n_components=2)
+        self.tsne_latent_space = tsne.fit_transform(self.latent_space_data)
+
+    def plot(self):
+        cloud_dots_figure, cloud_dots_axis = plt.subplots()
+        cloud_dots_figure.canvas.manager.set_window_title('Latent Space')
+        scatter = cloud_dots_axis.scatter(self.tsne_latent_space[:, 0], self.tsne_latent_space[:, 1], c=self.latent_space_color)
+
+        figure_rendering, axis_rendering = plt.subplots()
+        figure_rendering.canvas.manager.set_window_title('Environment')
+        axis_rendering.axis('off')
+
+        def on_hover(event):
+            if event.inaxes == cloud_dots_axis:
+                contains, ind = scatter.contains(event)
+                if contains:
+                    index = ind['ind'][0]
+                    axis_rendering.clear()
+                    axis_rendering.imshow(self.rendering_data[index])
+                    plt.draw()
+                else:
+                    axis_rendering.clear()
+                    axis_rendering.axis('off')
+                    plt.draw()
+
+        cloud_dots_figure.canvas.mpl_connect('motion_notify_event', on_hover)
+        plt.show()
 
 
 if __name__ == '__main__':
-    latent_space_dataset_handler: DatasetHandler = DatasetHandler(datasets_directory, 'latent_space')
-    latent_space_dataset_handler.print_info()
-    data = latent_space_dataset_handler.load(['latent_space', 'action', 'rendering'], number_data=100)
+    latent_space_visualisation = LatentSpaceVisualisation('latent_space')
+    latent_space_visualisation.load_data(50)
+    latent_space_visualisation.load_kmeans_classifier('kmeans_classifier')
+    latent_space_visualisation.kmeans_classifier_predict()
+    latent_space_visualisation.compute_tsne()
+    latent_space_visualisation.plot()
 
-    latent_space_dataset = data['latent_space']
-    action_dataset = data['action']
-    rendering_dataset = data['rendering']
-    # color = np.where(action_dataset == 0, 'r', 'b')
 
-    # pca = PCA(n_components=2)
-    # vector_2d_pca = pca.fit_transform(latent_space_dataset)
 
-    kmeans_classifier = SklearnClassifierHandler(sklearn_directory, 'kmeans_classifier').load()
-    cluster_assigned_label = kmeans_classifier.predict(latent_space_dataset)
-
-    # number_label = kmeans_classifier.n_clusters
-    # color_palette = plt.cm.get_cmap('tab10', number_label)
-    color_palette = matplotlib.colormaps['tab10']
-    color = [color_palette(label) for label in cluster_assigned_label]
-
-    tsne = TSNE(n_components=2)
-    vector_2d_tsne = tsne.fit_transform(latent_space_dataset)
-
-    # plt.figure(figsize=(12, 6))
-
-    # plt.subplot(1, 2, 1)
-    # plt.scatter(vector_2d_pca[:, 0], vector_2d_pca[:, 1], c=colors, marker='o', s=50)
-    # plt.title('PCA')
-    #
-    # plt.subplot(1, 2, 2)
-    # plt.scatter(vector_2d_tsne[:, 0], vector_2d_tsne[:, 1], c=colors, marker='o', s=50)
-    # plt.title('t-SNE')
-
-    # plt.subplot(1, 2, 2)
-    # plt.scatter(latent_space_data[:, 0], latent_space_data[:, 1], c=colors, marker='o', s=50)
-    # plt.title('Base')
-
-    # plt.show()
-
-    plot_latent_space(vector_2d_tsne[:, 0], vector_2d_tsne[:, 1], color, rendering_dataset)
